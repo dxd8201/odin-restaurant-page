4cd9bb887eea91c089ee798b04c4d584c0ef7f15
dxd8201 <ddronet17@gmail.com>
2023-05-11 17:37:36 -0500

    fixed bug preventing DOM content from loading

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import './style.css';
+
 console.log("Hi I'm Tom.");
+
+const containerDiv = (() => {
+    const element = document.createElement('div');
+    element.innerHTML = "Hi dad";
+
+    document.body.appendChild(element);
+    return
+})();
+
+console.log(containerDiv)
